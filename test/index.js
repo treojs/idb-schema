3a30d29c94dfbe8a6386e66aa71b810bf9fa47dc
Aleksey Kulikov <alekseys.kulikov@gmail.com>
2015-11-28 10:00:20 +0100

    improve tests with idb-factory and idb-request

--- test/index.js
+++ test/index.js
@@ -1,32 +1,21 @@
-import treoWebsql from 'treo-websql'
+import 'indexeddbshim'
+import ES6Promise from 'es6-promise'
 import { expect } from 'chai'
 import { pluck, toArray } from 'lodash'
+import { del, open } from 'idb-factory'
+import { request } from 'idb-request'
 import Schema from '../src'
-
-treoWebsql.polyfill()
-const idb = global.indexedDB
-const dbName = 'mydb'
 
 describe('idb-schema', function idbSchemaTest() {
   this.timeout(5000)
+  ES6Promise.polyfill()
+  const dbName = 'mydb'
   let db
 
-  before(clean)
-  afterEach(clean)
+  before(() => del(dbName))
+  afterEach(() => del(db))
 
-  function clean(done) {
-    if (db) {
-      db.close()
-      db = null
-    }
-    setTimeout(() => {
-      const req = idb.deleteDatabase(dbName)
-      req.onerror = done
-      req.onsuccess = () => done()
-    }, 100)
-  }
-
-  it('describes database', (done) => {
+  it('describes database', () => {
     const schema = new Schema()
     .addStore('modules', { key: 'name' })
     .addIndex('byKeywords', 'keywords', { multiEntry: true })
@@ -46,11 +35,8 @@ describe('idb-schema', function idbSchemaTest() {
     expect(schema.stores()[0].indexes).length(4)
     expect(schema.stores()[1]).eql({ name: 'users', indexes: [], keyPath: 'id', autoIncrement: true })
 
-    const req = idb.open(dbName, schema.version())
-    req.onupgradeneeded = schema.callback()
-    req.onerror = done
-    req.onsuccess = (e1) => {
-      db = e1.target.result
+    return open(dbName, schema.version(), schema.callback()).then((originDb) => {
+      db = originDb
       expect(db.version).equal(1)
       expect(toArray(db.objectStoreNames)).eql(['modules', 'users'])
 
@@ -74,14 +60,13 @@ describe('idb-schema', function idbSchemaTest() {
         expect(modules.index('byAuthor').multiEntry).equal(false)
       }
 
-      users.count().onsuccess = (e2) => {
-        expect(e2.target.result).equal(3)
-        done()
-      }
-    }
+      return request(users.count()).then((count) => {
+        expect(count).equal(3)
+      })
+    })
   })
 
-  it('enables cascading migrations', (done) => {
+  it('enables cascading migrations', () => {
     const schema = new Schema()
     .version(1)
       .addStore('books', { keyPath: 'isbn' })
@@ -95,35 +80,26 @@ describe('idb-schema', function idbSchemaTest() {
       .addIndex('byPublisher', 'publisher')
       .addIndex('byFrequency', 'frequency')
 
-    const req1 = idb.open(dbName, schema.version())
-    req1.onupgradeneeded = schema.callback()
-    req1.onerror = done
-    req1.onsuccess = (e1) => {
-      db = e1.target.result
+    return open(dbName, schema.version(), schema.callback()).then((originDb) => {
+      db = originDb
       expect(db.version).equal(3)
       expect(toArray(db.objectStoreNames)).eql(['books', 'magazines'])
 
       db.close()
-      setTimeout(() => {
-        schema.version(4)
-        .delStore('books')
-        .getStore('magazines')
-        .delIndex('byPublisher')
+      schema.version(4)
+      .delStore('books')
+      .getStore('magazines')
+      .delIndex('byPublisher')
 
-        const req2 = idb.open(dbName, schema.version())
-        req2.onupgradeneeded = schema.callback()
-        req2.onerror = done
-        req2.onsuccess = (e2) => {
-          db = e2.target.result
-          expect(db.version).equal(4)
-          expect(toArray(db.objectStoreNames)).eql(['magazines'])
+      return open(dbName, schema.version(), schema.callback()).then((originDb2) => {
+        db = originDb2
+        expect(db.version).equal(4)
+        expect(toArray(db.objectStoreNames)).eql(['magazines'])
 
-          const magazines = db.transaction(['magazines'], 'readonly').objectStore('magazines')
-          expect(toArray(magazines.indexNames)).eql(['byFrequency'])
-          done()
-        }
-      }, 100)
-    }
+        const magazines = db.transaction(['magazines'], 'readonly').objectStore('magazines')
+        expect(toArray(magazines.indexNames)).eql(['byFrequency'])
+      })
+    })
   })
 
   it('#clone', () => {
